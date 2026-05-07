ca2c66c83b78081b9864f7b788046128a8b9e985
Mick <mdmorales@gbox.adnu.edu.ph>
2026-05-07 12:56:46 +0800

    Fix: Missing JWT imports causing 500 error

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import json
 import requests
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from passlib.hash import pbkdf2_sha256
 
 # --- CONFIG ---
